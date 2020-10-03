1e55f7783f08edbf917f833603987b8441186077
Forest <linshs@qq.com>
2020-10-03 17:21:47 +0800

    测试Python脚本

---
+++ install-homebrew-china.py
@@ -0,0 +1,25 @@
+# -*- coding:  utf-8 -*-
+import os,sys,subprocess
+curFileDir = os.path.dirname(__file__)
+installShFileName = 'install222.sh'
+
+#给install.sh加权限
+# print('给' + installShFileName + '加执行权限，需要输入电脑密码: ')
+# pipe = subprocess.Popen("sudo chmod +x " + curFileDir + "/" + installShFileName, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+# (outData , errData) = pipe.communicate()
+# if pipe.returncode <= 1:
+#     print('OK')
+#     print(outData.decode())
+# else:
+#     print(errData.decode())
+
+
+#运行install.sh
+print('开始运行'+installShFileName)
+pipe = subprocess.Popen(["sh", curFileDir + "/" + installShFileName], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+(outData , errData) = pipe.communicate()
+if pipe.returncode <= 1:
+    print('OK')
+    print(outData.decode())
+else:
+    print(errData.decode())
