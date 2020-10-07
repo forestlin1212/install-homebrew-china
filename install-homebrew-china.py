e20a2f220b8f50a37cb78855b2b689c4b86da24c
Forest <linshs@qq.com>
2020-10-08 01:07:18 +0800

    测试版Python脚本
    有个问题是，无法真正捕捉输出文字。有的命令过程输出的的文字不在pipe.stdout里，不知道在什么地方。

--- install-homebrew-china.py
+++ install-homebrew-china.py
@@ -1,38 +1,115 @@
 # -*- coding:  utf-8 -*-
-import os,sys,subprocess
+import os
+import sys
+from subprocess import PIPE, Popen
+
 curFileDir = os.path.dirname(__file__)
-installShFileName = 'install222.sh'
+ustcSource = 'mirrors.ustc.edu.cn'
+installShFileName = '222.sh'
+
+def runCommand(command, endOutpuptStr=""):
+    """
+    执行一个命令，实时打印命令的输出。直到命令执行结束。
+    command格式: eg. ls -all, 则传入数组 ["ls","-all"]
+    如果输出了endOutpuptStr开头的文字，则终止命令。
+    返回是否执行成功。
+    """
+    isSucess = True
+    print('endOutpuptStr:'+endOutpuptStr)
+    with Popen(command, stdout=PIPE, bufsize=1, universal_newlines=True) as pipe:
+        # for line in pipe.stdout:
+        #     print(line)
+        #     print(line, end='')
+            
+        #     if endOutpuptStr != "" and line.find(endOutpuptStr) != -1:
+        #         print('强制结束命令')
+        #         pipe.terminate()
+        #         break
+
+        # if pipe.wait() > 0:
+        #     isSucess = False
+        (outData, ErrData) = pipe.communicate()
+        print("out:::::::::::::::::"+ErrData.decode())
+    return isSucess
+
+def runCommandSerial(command, endOutpuptStr=""):
+    """
+    执行一个命令，实时打印命令的输出。直到命令执行结束。
+    command格式: eg. ls -all, 则传入数组 ["ls","-all"]
+    如果输出了endOutpuptStr开头的文字，则终止命令。
+    如果最终命令执行失败，则退出程序。
+    """
+    isSucess = runCommand(command, endOutpuptStr)
+    if not isSucess:
+        exit()
+
+def getCurrentShellName():
+    """
+    获取当前shell是zsh还是bash
+    """
+    pipe = Popen("echo $SHELL", shell=True, stdin=PIPE, stdout=PIPE)
+    (outData , errData) = pipe.communicate()
+    shellStr = outData.decode().strip()
+    if shellStr.endswith("zsh"):
+        return "zsh"
+    elif shellStr.endswith("bash"):
+        return "bash"
+    else:
+        return "other"
+
+runCommandSerial(["git","clone"])
+#test
+exit()
 
 #给install.sh加权限
-# print('给' + installShFileName + '加执行权限，需要输入电脑密码: ')
-# pipe = subprocess.Popen("sudo chmod +x " + curFileDir + "/" + installShFileName, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-# (outData , errData) = pipe.communicate()
-# if pipe.returncode <= 1:
-#     print('OK')
-#     print(outData.decode())
-# else:
-#     print(errData.decode())
+print('给' + installShFileName + '加执行权限，需要输入电脑密码: ')
+runCommandSerial(["sudo", "chmod", "+x", curFileDir + "/" + installShFileName])
+
+#运行install.h，到“Cloning into”时，停止。
+print('开始运行' + installShFileName)
+runCommandSerial(["sh", curFileDir + "/" + installShFileName],
+           endOutpuptStr="Cloning into")
+
+#手动运行git clone命令
+print('把homebrew-core 运行git clone到本地')
+runCommandSerial([
+    "git", "clone", "git://" + ustcSource + "/homebrew-core.git/",
+    "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core", "--depth=1"
+])
+
+#更换shell的bottles
+print('更换bottles')
+shellName = getCurrentShellName()
+if shellName == "zsh":
+    runCommandSerial([
+        "echo",
+        "\'export HOMEBREW_BOTTLE_DOMAIN=https://" + ustcSource + "/homebrew-bottles\'",
+        ">>", "~/.zshrc"
+    ])
+elif shellName == "bash":
+    runCommandSerial([
+        "echo",
+        "\'export HOMEBREW_BOTTLE_DOMAIN=https://" + ustcSource + "/homebrew-bottles\'",
+        ">>", "~/.bash_profile"
+    ])
+else:
+    print('你的shell不是zsh，也不是bash。无法更换国内bottles源！！！')
 
 
-#运行install.sh
-def runInstallSh() :
-    print('开始运行'+installShFileName)
-    pipe = subprocess.Popen(["sh", curFileDir + "/" + installShFileName], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    
-    # while(True):
-    #     # returns None while subprocess is running
-    #     retcode = pipe.poll() 
-    #     line = pipe.stdout.readline()
-    #     yield line    
-    #     if retcode is not None:
-    #         break
 
-    (outData , errData) = pipe.communicate()
-    if pipe.returncode <= 1:
-        print('OK')
-        print(outData.decode())
-    else:
-        print(errData.decode())
+#更换brew源
+print('更换brew源')
+runCommandSerial(["cd", "\"$(brew --repo)\""])
+runCommandSerial([
+    "git", "remote", "set-url", "origin", "https://" + ustcSource + "/brew.git"
+])
 
+#更换homebrew-core源
+print('更换homebrew-core源')
+runCommandSerial(["cd", "\"$(brew --repo)/Library/Taps/homebrew/homebrew-core\""])
+runCommandSerial([
+    "git", "remote", "set-url", "origin",
+    "https://" + ustcSource + "/homebrew-core.git"
+])
 
-runInstallSh()
+print('完成，输入brew update，试试')
--- install222.sh
+++ install222.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ls
+echo "00000"
+sleep 2
+echo "11111"
+sleep 2
+adb shell
